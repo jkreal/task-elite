50c383bf95cbab809599987ef20083a82642bc37
Andrew Johnson <32281250+andrewj12763@users.noreply.github.com>
2018-06-22 15:12:56 -0600

    login page layout

--- client/src/App.js
+++ client/src/App.js
@@ -17,13 +17,8 @@ import Sidebar from "./Components/Sidebar/Sidebar";
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
-      <Login />
-      
-=======
       <Sidebar />
 
->>>>>>> 702877e33fab33629fa17ec50d38e07136aecfc4
     );
   }
 }
