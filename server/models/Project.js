8f4e9b83b96df7578e52d65695a1aa6ae9b9a551
jkrowden <jkrowden@gmail.com>
2018-06-21 21:25:46 -0600

    database relations set up

--- server/models/Project.js
+++ server/models/Project.js
@@ -1,29 +1,31 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
 	var Project = sequelize.define("Project", {
-		 id:  { 
-				type: DataTypes.INTEGER, 
-				autoIncrement: true, 
-				primaryKey: true
-			},
-			projectName: DataTypes.STRING,
-			description: {
-				type: DataTypes.STRING,
-				notNull: true
-			}
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true
+		},
+		projectName: DataTypes.STRING,
+		description: {
+			type: DataTypes.STRING,
+			notNull: true
+		}
 	}, {
-			tableName: 'projects',
-			timestamps: false,
-			classMethods: {
-					associate : function(models) {
-							Project.belongsTo(models.User, {foreignKey: 'user_id'})
-							Project.hasMany(models.Department, {foreignKey: 'department_id'})
-							Project.hasMany(models.User, {foreignKey: 'user_id'})
-					},
-				},
+		tableName: 'projects',
+		timestamps: false,
+	});
+
+	Project.associate = function (models) {
+		models.Project.belongsTo(models.User, {
+			foreignKey: 'owner_id'
 		});
-	
-	
-	
+		models.Project.hasMany(models.Department, {
+			foreignKey: 'department_id'
+		});
+	}
+
+
+
 	return Project;
 
 };
--- server/models/user.js
+++ server/models/user.js
@@ -1,48 +1,42 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
 	var User = sequelize.define("User", {
-		 		id: {
-			autoIncrement: true,
-			primaryKey: true,
-			type: DataTypes.INTEGER
-		},
-		fullname: {
-			type: DataTypes.STRING,
-			notEmpty: true
-		},
-		username: {
-			type: DataTypes.TEXT
-		},
-		email: {
-			type: DataTypes.STRING,
-			validate: {
-				isEmail: true
+			id: {
+				autoIncrement: true,
+				primaryKey: true,
+				type: DataTypes.INTEGER
+			},
+			fullname: {
+				type: DataTypes.STRING,
+				notEmpty: true
+			},
+			username: {
+				type: DataTypes.TEXT
+			},
+			email: {
+				type: DataTypes.STRING,
+				validate: {
+					isEmail: true
+				}
+			},
+			password: {
+				type: DataTypes.STRING,
+				allowNull: false
 			}
-		},
-		password: {
-			type: DataTypes.STRING,
-			allowNull: false
 		}
-	}
-		
+
 		, {
 			tableName: 'users',
 			timestamps: false,
-			classMethods: {
-					associate : function(models) {
-							User.hasMany(Project, {foreignKey: 'project_id'});
-					},
-				},
 		});
-	
-	
-	
+
+	User.associate = function (models) {
+		models.User.hasMany(models.Project, {
+			foreignKey: 'project_id'
+		});
+	}
+
+
+
 	return User;
 
 };
-
-
-
-
-
-
-
