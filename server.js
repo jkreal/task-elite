7045fe60eec92fd4043b7598024453564406d732
jkrowden <jkrowden@gmail.com>
2018-06-21 18:45:01 -0600

    auth

--- server.js
+++ server.js
@@ -1,19 +1,72 @@
-const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
-const app = express();
+var express = require('express');
+var app = express();
+var passport = require('passport');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var env = require('dotenv').load();
+var exphbs = require('express-handlebars');
+
+var PORT = process.env.PORT || 3001;
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
+
+
+// For Passport
+app.use(session({
+	secret: 'keyboard cat',
+	resave: true,
+	saveUninitialized: true
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+
+app.set('views', './server/views')
+app.engine('hbs', exphbs({
+	extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+
+//Models
+var models = require("./server/models");
+
+
+//Routes
+var authRoute = require('./server/routes/auth.js')(app, passport);
+
+
+//load passport strategies
+require('./server/config/passport/passport.js')(passport, models.user);
+
+
+//Sync Database
+models.sequelize.sync().then(function () {
+	console.log('Nice! Database looks fine')
+
+}).catch(function (err) {
+	console.log(err, "Something went wrong with the Database Update!")
+});
+
+
+app.get('/', function (req, res) {
+	res.send('Welcome to Passport with Sequelize');
+});
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+	app.use(express.static("client/build"));
 }
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.get("*", function (req, res) {
+	res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> Server now on port ${PORT}!`);
+app.listen(PORT, function () {
+	console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
