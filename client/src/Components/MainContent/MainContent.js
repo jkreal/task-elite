14f67c887f3456f25efc94eb1b603904fa674ae0
Spencer Daniels <speencah@anc.elen.utah.edu>
2018-06-23 12:57:35 -0600

    Main content and Dashboard styling

---
+++ client/src/Components/MainContent/MainContent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Users from "./Users";
+import "./MainContent.css";
+
+const MainContent = (props) => (
+    <div>
+        <Users />
+        
+      </div>
+)
+
+export default MainContent;
