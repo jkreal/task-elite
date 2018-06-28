111961fb9d00ffc18875c3c377d95fa3f6994f76
vengefulbliss <zapyaplee@gmail.com>
2018-06-28 11:50:46 -0600

    updating to pull.

--- client/src/Components/MainContent/MainContent.js
+++ client/src/Components/MainContent/MainContent.js
@@ -1,10 +1,11 @@
+
 import React from "react";
-import Users from "./Users";
+import Tasks from "../Tasks";
 import "./MainContent.css";
 
 const MainContent = (props) => (
     <div>
-        <Users />
+        <Tasks />
         
       </div>
 )
