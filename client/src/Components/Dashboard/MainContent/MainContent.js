c095326759a231280af5453f02d52fff93554803
jkrowden <jkrowden@gmail.com>
2018-07-03 16:23:50 -0600

    heroku build

--- client/src/Components/Dashboard/MainContent/MainContent.js
+++ client/src/Components/Dashboard/MainContent/MainContent.js
@@ -7,22 +7,18 @@ import "./MainContent.css";
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const MainContent = (props) => (
-
-    <div className="maincontentContainer">
-        {/* <Users /> */}
-        <Departments />
-        {/* <MainTasks /> */}
-      </div>
-
+<div>
 	<div className="maincontentContainer">
-		{/*This is where the react router will decide which component to load*/}
 		{/* <Users /> */}
-		{/* <Departments /> */}
-			<Route path="/tasks" component={MainTasks}/>
-			<Route path="/users" component={Users}/>
-
+		<Departments />
+		{/* <MainTasks /> */}
 	</div>
 
+	<div className="maincontentContainer">
+		<Route path="/tasks" component={MainTasks} />
+		<Route path="/users" component={Users} />
+	</div>
+</div>
 )
 
 export default MainContent;
